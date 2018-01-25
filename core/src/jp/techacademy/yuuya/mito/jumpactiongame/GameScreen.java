c6d9983abe136cbd9a85d3fcaed8656176339f9e
Yuya@Windows <yuya.m.tokai@gmail.com>
2018-01-25 18:34:47 +0900

    2nd Commit

--- core/src/jp/techacademy/yuuya/mito/jumpactiongame/GameScreen.java
+++ core/src/jp/techacademy/yuuya/mito/jumpactiongame/GameScreen.java
@@ -9,15 +9,37 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameScreen extends ScreenAdapter {
     static final float CAMERA_WIDTH = 10;
     static final float CAMERA_HEIGHT = 15;
+
+    static final float WORLD_WIDTH = 10;
+    static final float WORLD_HEIGHT = 15 * 20;//20画面分登れば終了
+
+    static final int GAME_STATE_READY = 0;
+    static final int GAME_STATE_PLAYING = 1;
+    static final int GAME_STATE_GAMEOVER = 2;
+
+    //重力
+    static final float GRAVITY = -12;
 
     private JumpActionGame mGame;
 
     Sprite mBg;
     OrthographicCamera mCamera;
     FitViewport mViewPort;
+
+    Random mRandom;
+    List<Step> mSteps;
+    List<Star> mStars;
+    Ufo mUfo;
+    Player mPlayer;
+
+    int mGameState;
 
     public GameScreen(JumpActionGame game){
         mGame = game;
@@ -33,10 +55,22 @@ public class GameScreen extends ScreenAdapter {
         mCamera = new OrthographicCamera();
         mCamera.setToOrtho(false,CAMERA_WIDTH,CAMERA_HEIGHT);
         mViewPort = new FitViewport(CAMERA_WIDTH,CAMERA_HEIGHT,mCamera);
+
+        //メンバ変数の初期化
+        mRandom = new Random();
+        mSteps = new ArrayList<Step>();
+        mStars = new ArrayList<Star>();
+        mGameState = GAME_STATE_READY;
+
+        createStage();
+
     }
 
     @Override
     public void render(float delta){
+        //それぞれをアップデートする
+        update(delta);
+
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -46,13 +80,46 @@ public class GameScreen extends ScreenAdapter {
 
         mGame.batch.begin();
 
+        //背景
         //原点は左下
         mBg.setPosition(mCamera.position.x - CAMERA_WIDTH/2,mCamera.position.y - CAMERA_HEIGHT/2);
         mBg.draw(mGame.batch);
 
+        //Step
+        for(int i = 0; i < mSteps.size(); i++){
+            mSteps.get(i).draw(mGame.batch);
+        }
 
-        mBg.draw(mGame.batch);
+        //Star
+        for(int i = 0; i < mStars.size(); i++){
+            mStars.get(i).draw(mGame.batch);
+        }
+
+        //UFO
+        mUfo.draw(mGame.batch);
+
+        //Player
+        mPlayer.draw(mGame.batch);
+
+
         mGame.batch.end();
+    }
+
+
+    //ステージを作成する
+    private void createStage(){
+        //テクスチャの準備
+        Texture stepTexture = new Texture("step.png");
+        Texture starTexture = new Texture("star.png");
+        Texture playerTexture = new Texture("uma.png");
+        Texture ufoTexture = new Texture("ugo.png");
+
+        //StepとStarをゴールの高さまで配置していく
+        float y = 0;
+
+        float maxJumpHeight = Player.PLAYER_JUMP_VELOCITY * Player.PLAYER_JUMP_VELOCITY / (2 * -GRAVITY);
+
+
     }
 
 }
